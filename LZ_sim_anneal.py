a59e1457187ec2e08818d4442d145d6e71282c14
Alexandre Day <alexandre.day1@gmail.com>
2016-10-21 15:43:43 -0400

    Updated simulated annealing, need to benchmark fidelity calculations

--- LZ_sim_anneal.py
+++ LZ_sim_anneal.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import Hamiltonian
 import numpy as np
+#import deepcopy
 
 
 
@@ -64,88 +65,130 @@ hx_f = 2.0 #+1.0 # final hx coupling
 # define ED Hamiltonian H(t)
 m=0.0
 b=hx_i
-lin_fun = lambda t: m*t + b
-# define Hamiltonian
-H, basis = Hamiltonian.Hamiltonian(L,fun=lin_fun,**{'J':J,'hz':hz})
-# defien Hamiltonian for interpolated fidelity
-t_vals, p_vals = [0.0,0.0], [0.0,0.0]
+#lin_fun = lambda t: m*t + b
+
+t_vals, p_vals = [0.0,1.0,2.0], [0.0,0.2,0.4]
 protocol_fun = lambda t: np.interp(t, t_vals, p_vals)
-H, basis = Hamiltonian.Hamiltonian(L,fun=lin_fun,**{'J':J,'hz':hz})
+
+#def lin_fun(t):
+    #print p_vals
+#    return np.interp(t, t_vals, p_vals)
+# define Hamiltonian
+
+H, basis = Hamiltonian.Hamiltonian(L,fun=protocol_fun,**{'J':J,'hz':hz})
+# defien Hamiltonian for interpolated fidelity
+#global t_vals,p_vals
+#protocol_fun = lambda t: np.interp(t, t_vals, p_vals)
+
+H_fid,_ = Hamiltonian.Hamiltonian(L,fun=protocol_fun,**{'J':J,'hz':hz})
+
+
 
 
 # calculate initial state
 E_i, psi_i = H.eigsh(time=0,k=1,sigma=-100.0)
 E_i = E_i.squeeze()
 psi_i = psi_i.squeeze()
-# calculate final state
+
 b = hx_f
 E_f, psi_f = H.eigsh(time=0,k=1,sigma=-100.0)
 E_f = E_f.squeeze()
 psi_f = psi_f.squeeze()
 
-print(Fidelity(psi_i,H,[t_vals[0],t_vals[-1]],basis=basis,psi_f=psi_f,all_obs=False)[-1])
+print(Fidelity(psi_i,H_fid,[t_vals[0],t_vals[-1]],basis=basis,psi_f=psi_f,all_obs=False)[-1])
+
+p_vals=[0.0,0.2,0.8]
+
+print(Fidelity(psi_i,H_fid,[t_vals[0],t_vals[-1]],basis=basis,psi_f=psi_f,all_obs=False)[-1])
+
+print(psi_i.shape)
+print(abs(psi_i.conj().dot(psi_f))**2)
+exit()
 
 
-#===============================================================================
-#===============================================================================
-# action_set=np.array([-0.2,0.1,-0.5,10.])
-# cp=action_set[np.random.choice([0,1,2,3],20)]
-# print(cp)
-# print(np.array([np.sum(cp[:t]) for t in range(20)]))r
-# exit()
-#===============================================================================
-#===============================================================================
+def random_trajectory(action_set,hi,episode_length):
+    '''
+    
+    Returns the action protocol and the corresponding trajectory
+    
+    '''
+    
+    action_protocol=np.random.choice(action_set,episode_length)
+    return action_protocol,np.insert(hi+np.cumsum(action_protocol),0,hi)
 
- 
-def simulate_anneal(Ti,dT,episode_time,action_set):
+def propose_new_trajectory(current_action_protocol,action_set,hi,episode_length):
+    new_action_protocol=np.copy(current_action_protocol)
+    rand_pos=np.random.randint(episode_length)
+    new_action_protocol[rand_pos]=np.random.choice(action_set)
+    
+    return new_action_protocol,np.insert(hi+np.cumsum(new_action_protocol),0,hi)
+
+
+action_set=[-0.2,0.,0.2]
+hi=-1.
+episode_length=6
+
+ap,traj=random_trajectory([-0.2,0.,0.2],-1.,6)
+print("Initial action protocol",ap)
+print("Initial trajectory",traj)
+
+ap_new,traj_new=propose_new_trajectory(ap,action_set,hi,episode_length)
+
+print("New action protocol",ap_new)
+print("New trajectory",traj_new)
+
+
+def simulate_anneal(Ti,dT,episode_length,action_set):
     T=Ti
     N=1000
-       
-    current_protocol=action_set[np.random.choice(action_set,episode_time)]
-    current_h_protocol=np.array([np.sum(current_protocol[:t]) for t in range(episode_time)])
-    
+    hi=-1.
+     
+    action_protocol,p_vals=random_trajectory(action_set,hi,episode_length)
+    t_vals=range(episode_length+1)
+    print(action_protocol)
+    print(p_vals)
+    print(t_vals)
+    #exit()
+     
     #current_state=
-       
+     
     while T>0.:
         beta=1./T
         for i in range(1000):
-            rand_pos=np.random.randint(L)
-            new_protocol=current_protocol
-            new_protocol[rand_pos]=action_set[np.random_choice(action_set,1)]
-            dF=-(Fidelity(new_protocol)-Fidelity(current_protocol))
             
+            action_protocol_new,p_vals_new=propose_new_trajectory(action_protocol,action_set,hi,episode_length)
+            
+            current_fid=Fidelity(psi_i,H_fid,[t_vals[0],t_vals[-1]],basis=basis,psi_f=psi_f,all_obs=False)[-1]
+            print(current_fid) 
+            print(p_vals)
+            
+            old_p_vals=p_vals[:]
+            p_vals=p_vals_new[:]
+            
+            new_fidelity=Fidelity(psi_i,H_fid,[t_vals[0],t_vals[-1]],basis=basis,psi_f=psi_f,all_obs=False)[-1]
+            print(p_vals)
+            print(new_fidelity)
+            dF=-(new_fidelity-current_fid)
+             
+            print(dF)
+            exit()
             if dF<0:
                 current_protocol=new_protocol
             elif np.random.uniform() < np.exp(-beta*dF):
                 current_protocol=new_protocol
         T-=dT
-    
+     
     return current_protocol
-        
+         
+action_set=[-0.2,0.0,0.2]
+ 
+simulate_anneal(100,0.01,10,action_set)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Fidelity(psi_i,H_fid,t_vals,basis=None,psi_f=None,all_obs=False)
-
-
-
-#psi_t = H_fid.evolve(psi_i,t_vals[0],t_vals,iterate=True,atol=1E-12,rtol=1E-12)
+print(np.random.choice(action_set))
+exit()
+simulate_anneal(100,0.1,20,action_set)
+ 
+ 
+ 
+ 
