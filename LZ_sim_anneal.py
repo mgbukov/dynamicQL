977813c301f197c3b799a4719f7753b20e7f12d4
Alexandre Day <alexandre.day1@gmail.com>
2017-01-01 15:19:15 -0500

    Commented and cleaned LZ_sim_anneal.py

--- LZ_sim_anneal.py
+++ LZ_sim_anneal.py
@@ -4,38 +4,66 @@ import Hamiltonian_alex as Hamiltonian
 from quspin.operators import exp_op
 import time
 import math
-import sys # for running in batch from terminal
+import sys,os # for running in batch from terminal
 from scipy.sparse.linalg import expm_multiply as expm
 #from matplotlib import pyplot as plt
-      
+
 np.set_printoptions(precision=4)
-          
+    
 def main():
     
     global action_set,hx_discrete,hx_max,FIX_NUMBER_FID_EVAL
     
+    """ 
+    Parameters
+        L: system size
+        J: Jzz interaction
+        hz: longitudinal field
+        hx_i: initial tranverse field coupling
+        hx_initial_state: initial state transverse field
+        hx_final_state: final state transverse field
+        N_quench: number of quenches (i.e. no. of discrete time evolutions)
+        delta_t: time scale 
+        N_restart: number of restart for the annealing
+        hx_max : maximum hx field (the annealer can go between -hx_max and hx_max
+        max_fid_eval: maximum number of fidelity evaluations
+        FIX_NUMBER_FID_EVAL: decide wether you want to fix the maximum number of fidelity evaluations
+        RL_CONSTRAINT: use reinforcement learning constraints or not
+        verbose: If you want the program to print to screen the progress
+    """
     L = 1 # system size
     J = 1.0/0.809 # zz interaction
     hz = 1.0 #0.9045/0.809 #1.0 # hz field
     hx_i = -4.0# -1.0 # initial hx coupling
     hx_initial_state= -1.0 # initial state
-    hx_f = 1.0 #+1.0 # final hx coupling
+    hx_final_state = 1.0 #+1.0 # final hx coupling
     N_quench=30
-    delta_t=0.01
+    delta_t=0.05
     N_restart=10
     hx_max=4
     max_fid_eval=1000
     FIX_NUMBER_FID_EVAL=False
+    RL_CONSTRAINT=True
+    verbose=True 
     
-    action_set1=[-5.0,0.,2.]
+    
+    print("-------------------- > Parameters < --------------------")
+    print("L \t\t\t %i\nJ \t\t\t %.3f\nhz \t\t\t %.3f\nhx(t=0) \t\t %.3f\nhx_max \t\t\t %.3f "%(L,J,hz,hx_i,hx_max))
+    print("hx_initial_state \t %.2f\nhx_final_state \t\t %.2f"%(hx_initial_state,hx_final_state))
+    print("N_quench \t\t %i\ndelta_t \t\t %.2f\nN_restart \t\t %i"%(N_quench,delta_t,N_restart))
+
+    action_set1=np.array([-8.0,0.,8.])
     action_set2=np.array([0.01,0.05,0.1,0.2,0.5,1.,2.,3.,4.],dtype=np.float32) # 17 actions in total
-    action_set2=list(np.round(np.concatenate((action_set2,-1.0*action_set2,[0])),3))
-    action_set3=[8.0,-8.0,0.0]
-    #action_set4=[-0.]
+    action_set2=np.sort(np.round(np.concatenate((action_set2,-1.0*action_set2,[0])),3))
+    action_set3=np.array([8.0,-8.0,0.0])
     
     if len(sys.argv)>1:
-        # argv order : Number of time step, action set number, filename for output max_fid_eval
-        # 
+        """ 
+            if len(sys.argv) > 1 : run from command line 
+        
+        """        
+        assert len(sys.argv) == 5, "Wrong number of parameters, expecting 4 parameters : n_step, n_action, action_set_number,outfile_name, max_number_fid"
+        
         N_time_step=int(sys.argv[1])
         action_set_no=sys.argv[2]
         action_set=eval('action_set'+action_set_no)
@@ -43,28 +71,57 @@ def main():
         max_fid_eval=int(sys.argv[4])
     else:
         N_time_step=20
-        outfile_name='BB_action_set_1'
+        outfile_name='first_test'
         action_set=action_set1
         max_fid_eval=3000
 
-    param={'J':J,'hz':hz,'hx':hx_i}
-    
-    # dynamical part at every time step (initiaze to zero everywhere) 
-    hx_discrete=[0]*N_time_step
+
+    print("N_time_step \t\t %i"%N_time_step)
+    print("Output file \t\t %s"%('data/'+outfile_name+".pkl"))
+    print("max_fid_eval (%s) \t\t %i"%(str(FIX_NUMBER_FID_EVAL),max_fid_eval))
+    print("Action_set -> %s"%np.round(action_set,3))
+    print("# of possible actions -> %i"%len(action_set))
+    print("---------------> Starting computation <----------------")
+
+    param={'J':J,'hz':hz,'hx':hx_i} # Hamiltonian kwargs 
+    hx_discrete=[0]*N_time_step # dynamical part at every time step (initiaze to zero everywhere)
     
     # full system hamiltonian
     H,_ = Hamiltonian.Hamiltonian(L,fct=hx_vs_t,**param)
    # print(H)
-    
+   
     # calculate initial and final states
     hx_discrete[0]=hx_initial_state # just a trick to get initial state
     E_i, psi_i = H.eigsh(time=0,k=1,which='SA')
-    hx_discrete[0]=hx_f # just a trick to get final state
+    hx_discrete[0]=hx_final_state # just a trick to get final state
     E_f, psi_target = H.eigsh(time=0,k=1,which='SA')
     hx_discrete[0]=0
     
-    print("Current action set:",action_set)
-    #hx_discrete=[-1.05, -0.050000000000000044, 0.95, 1.95, 1.95, 1.9, 1.9, 1.9, 1.9, 0.8999999999999999, -0.10000000000000009, -1.1, -1.3, -1.4000000000000001, -1.3800000000000001, -1.4800000000000002, -1.9800000000000002, -1.9900000000000002, -2.0, -2.0]
+    #compute_matrix_exp() accelerate dynamics, here !
+    
+    #print("Current action set:",np.array(action_set))
+    #print("Choosing random trajectory:")
+    
+    #action_protocol,hx_discrete=random_trajectory(hx_i,N_time_step)
+    #===========================================================================
+    # for a in action_set:
+    #     print(is_element_of_set(a, action_set))
+    #===========================================================================
+    
+    #===========================================================================
+    # print(hx_discrete)
+    # print(action_protocol)
+    # for i in np.arange(N_time_step):
+    #     print(i,avail_action(i,action_protocol,hx_discrete,hx_i,N_time_step))
+    # 
+    #===========================================================================
+    #print(3,avail_action(3,action_protocol,hx_discrete,hx_i,N_time_step))
+    #exit()
+    
+    # Check protocol
+    #hx_discrete=[ 4.,4.,4.,4.,4.,4.,4.,-4.,4.,-4.,4.,-4.,4,-4.,-4.,-4.,-4.,-4.,-4.,-4.]
+    #print(Fidelity(psi_i,H,N_time_step,delta_t,psi_target))
+    #exit()
     #N_time_step=len(hx_discrete)
     #hx_discrete=hx_discrete[:-1]
     #print(len(hx_discrete))
@@ -75,7 +132,8 @@ def main():
     # 
     # hx_discrete=[ 1.,1.,1.,1.,1.,-1.,-1.,-1.,-1.,-1.]
     # print(Fidelity(psi_i,H,N_time_step,delta_t,psi_target))
-    # test=np.loadtxt("data/text.dat",delimiter="\t")
+    # test=np.loadtxt("data/text.dat",delimiter="\t")h
+    
     # fbest=0.0
     # i=0
     # 
@@ -92,28 +150,38 @@ def main():
     # exit()
     # #test=np.loadtxt("data/text.dat",delimiter="\t")
     #===========================================================================
+    
+    
     sweep_size=N_time_step*len(action_set)
     if FIX_NUMBER_FID_EVAL:
+        # Determines the number of quenches needed to anneal w/o exceeding the max number of fidelity evaluations.
         N_quench=(max_fid_eval-5*sweep_size)//sweep_size
         assert N_quench >= 0
     print("Using N_quench=:%d"%N_quench)
     
+    # simulated annealing kwargs:
     param_SA={'Ti':0.04,'sweep_size':sweep_size,
                 'psi_i':psi_i,'H':H,'N_time_step':N_time_step,
                 'delta_t':delta_t,'psi_target':psi_target,
-                'hx_i':hx_i,'N_quench':N_quench
+                'hx_i':hx_i,'N_quench':N_quench,'RL_CONSTRAINT':RL_CONSTRAINT,
+                'verbose':verbose
                 }
-
-    
+        
     all_results=[]
+    
     for it in range(N_restart):
-       # print("Iteration:",it)
+        
         result=simulate_anneal(param_SA)
-        print(result)
+        
+        print("Result for iteration %i"%i,result)
+        
         all_results.append(result)
-        pkl_file=open('data/%s.pkl'%outfile_name,'wb')
-        pickle.dump(all_results,pkl_file)
-        pkl_file.close()
+        
+        with open('data/%s.pkl'%outfile_name,'wb') as pkl_file:
+            pkl_file.dump(all_results,pkl_file);pkl_file.close()
+            
+        print("Saved iteration --> %i to %s"%(it,'data/%s.pkl'%outfile_name))
+
         #print(result[0])
         #print(result[1])
         #print(result[2])
@@ -132,13 +200,15 @@ def main():
 #pkl_file=open('data/%s.pkl'%outfile_name,'wb')
 #pickle.dump(all_results,pkl_file)
     pkl_file.close()
-    
+
     
 def Fidelity(psi_i,H,N_time_step,delta_t,psi_target):
     """
-    Calculates final fidelity by evolving psi_i over a N_time_step 
-    Returns the overlap between the target state psi_target and the evolved state
-    
+    Purpose:
+        Calculates final fidelity by evolving psi_i over a N_time_step 
+    Return: 
+        Norm squared between the target state psi_target and the evolved state (according to the full hx_discrete protocol)
+        
     """    
     psi_evolve=psi_i.copy()
     for t in range(N_time_step):
@@ -147,19 +217,15 @@ def Fidelity(psi_i,H,N_time_step,delta_t,psi_target):
     #print(psi_evolve)
     return abs(np.sum(np.conj(psi_evolve)*psi_target))**2
 
-def Fidelity_fast(psi_i,H,N_time_step,delta_t,psi_target):
-    # Useful for small systems size)
-    psi_evolve=psi_i.copy()
-    for t in range(N_time_step):
-       psi_evolve=expm(-1j*delta_t*(H(time=t).todense()),psi_evolve)
-    return abs(np.dot(np.transpose(np.conj(psi_evolve)),psi_target)[0,0])**2
-
-   
+# Returns the hx field at a given time slice 
 def hx_vs_t(time): return hx_discrete[int(time)]
 
 def random_trajectory(hx_i,N_time_step):
     '''
-    Returns the action protocol and the corresponding trajectory
+    Purpose:
+        Generates a random trajectory
+    Return:
+        Action protocol,hx_discrete ; action taken at every time slice, field at every time slice
     '''
     action_protocol=[]
     current_h=hx_i
@@ -173,72 +239,134 @@ def random_trajectory(hx_i,N_time_step):
             else:
                 current_h-=action_choice
     
-    #action_protocol=np.random.choice(action_set,N_time_step)
     return action_protocol,hx_i+np.cumsum(action_protocol)
 
-def propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step):
+# Check if two floats are equal according to some precision
+def are_equal(a,b,prec=0.000001):
+    return abs(a-b)<prec
+
+# Check if a is in set by comparing floats (with a given precision)
+def is_element_of_set(a,set,prec=0.000001):
+    return np.min(abs(set-a)) < prec
+
+def avail_action_RL(time_position,old_action_protocol,old_hx_discrete,hx_i,N_time_step): # simulate SA in the space of actions, not field... need to recompute traj. 
+    """
+    Purpose:
+        Get available actions at a specific time slice given a protocol and with RL constratins 
+    Return:
+        A list of available actions
+    """
+    action_subset=[]
+    if time_position==N_time_step-1:
+        h_pre=old_hx_discrete[time_position-1]
+        for a in action_set:
+            if abs(a+h_pre) < hx_max+0.00001:
+                action_subset.append(a)
+    else:
+        # General case
+        h_next=old_hx_discrete[time_position+1]
+        if time_position==0:
+            h_pre=hx_i
+        else:
+            h_pre=old_hx_discrete[time_position-1]
+        dh=h_next-h_pre
+        for a in action_set:
+            if is_element_of_set(dh-a,action_set):
+                if abs(a+h_pre) < hx_max+0.00001:
+                    action_subset.append(a)
+    return action_subset
+
+def avail_action(time_position,old_action_protocol,old_hx_discrete,hx_i,N_time_step):
+    """
+    Purpose:
+        Get available actions at a specific time slice given a protocol without any constraints (except having abs(hx)<abs(hx_max) 
+    Return:
+        A list of available actions
+    """
+    action_subset=[]
+    if time_position==N_time_step-1:
+        h_pre=old_hx_discrete[time_position-1]
+        for a in action_set:
+            if abs(a+h_pre) < hx_max+0.00001:
+                action_subset.append(a)
+    else:
+        # General case
+        if time_position==0:
+            h_pre=hx_i
+        else:
+            h_pre=old_hx_discrete[time_position-1]
+        for a in action_set:
+            if abs(a+h_pre) < hx_max+0.00001:
+                action_subset.append(a)
+    return action_subset
+
+def propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step,RL_constraint=False):
     '''
-    Given the old_action_protocol, makes a random change and returns the new action protocol
+    Purpose:
+        Given the old_action_protocol, makes a random change and returns the new action protocol
+        
+    Return:
+        New action protocol,New hx as a function of the time slice
     '''
     new_hx_discrete=np.copy(old_hx_discrete)
     rand_pos=np.random.randint(N_time_step)
-    aset=np.array(action_set)
+    current_action=old_action_protocol[rand_pos]
+    
+    tmp=[]
     count=0
-    option=0
     # w/o RL constraints
-    if option == 0: 
-        while True:
-            a=np.random.choice(action_set)
-            if abs(a) > 1e-6:
-                r=new_hx_discrete[rand_pos]+a
-                if abs(r) < hx_max+0.00001:
-                    new_hx_discrete[rand_pos]+=a
-                    break
+    while True:
+        if RL_constraint:
+            aset=avail_action_RL(rand_pos,old_action_protocol,old_hx_discrete,hx_i,N_time_step)
+        else:
+            aset=avail_action(rand_pos,old_action_protocol,old_hx_discrete,hx_i,N_time_step)
+        for aa in aset:
+            if not are_equal(aa,current_action):
+                tmp.append(aa)
+    
+        if len(tmp)==0:
+            rand_pos=np.random.randint(N_time_step)
+            current_action=old_action_protocol[rand_pos]
+        else:
+            a=np.random.choice(tmp)
+            break
+    
+    if rand_pos==0:
+        h_pre=hx_i
     else:
-        while True:
-            count+=1
-            a=np.random.choice(action_set)
-            if abs(a) > 1e-6:
-                r=new_hx_discrete[rand_pos]+a
-                if rand_pos != 0 and rand_pos != N_time_step-1:
-                    dhpre=np.min(np.abs(aset-abs(r-new_hx_discrete[rand_pos-1])))
-                    dhpost=np.min(np.abs(aset-abs(r-new_hx_discrete[rand_pos+1])))
-                    
-                    if abs(r) < 2.0001 and dhpre < 0.00001 and dhpost < 0.00001:
-                        new_hx_discrete[rand_pos]+=a
-                        break
-                elif rand_pos == 0:
-                    dhpre=np.min(np.abs(aset-abs(r-hx_i)))
-                    dhpost=np.min(np.abs(aset-abs(r-new_hx_discrete[rand_pos+1])))
-                    if abs(r) < 2.0001 and dhpre < 0.00001 and dhpost < 0.00001:
-                        new_hx_discrete[rand_pos]+=a
-                        break
-                else:
-                    dhpre=np.min(np.abs(aset-abs(r-new_hx_discrete[rand_pos-1])))
-                    if abs(r) < 2.0001 and dhpre < 0.00001:
-                        new_hx_discrete[rand_pos]+=a
-                        break
-                    
-            if count > 10:
-                rand_pos=np.random.randint(N_time_step)
-    #new_action_protocol=np.copy(old_action_protocol)
+        h_pre=old_hx_discrete[rand_pos-1]
+
+    new_hx_discrete[rand_pos]=h_pre+a
     new_action_protocol=np.diff(new_hx_discrete)
     new_action_protocol=np.concatenate(([new_hx_discrete[0]-hx_i],new_action_protocol))
     return new_action_protocol,new_hx_discrete
-    #new_action_protocol[rand_pos]=np.random.choice(action_set)
-    #print(N_tim)
-    #return new_action_protocol,hx_t
     
 
 def simulate_anneal(params):
+    """
+    Purpose:
+        Performs simulated annealing by trying to maximize the fidelity between a state evolved
+        using a quantum time evolution protocol and a predefined target state.
+    Return:
+        Number of times of fidelity evaluation,
+        Best obtained fidelity,
+        Corresponding action protocol,
+        Corresponding hx fiels vs time slice"
+    """
     
     global hx_discrete
     
+    if params['verbose']:
+        enablePrint()
+    else:
+        blockPrint()
+    
+
     # Simulated annealing parameters
     T=params['Ti']
     Ti=T
     N_quench=params['N_quench']
-    #max_fid_eval=params['max_fid_eval']
+    RL_constraint=params['RL_CONSTRAINT']
     step=0.0
     sweep_size=params['sweep_size']
     beta=1./T
@@ -267,13 +395,12 @@ def simulate_anneal(params):
     
     while T>1E-6:
         if N_quench==0:break
-        #print(T,best_fid)
+    
         print("Current temperature: %.4f\tBest fidelity: %.4f\tFidelity count: %i"%(T,best_fid,count_fid_eval))
         
-        #print("Current temperature=%s"%(1./beta),"Best fidelity=%s"%best_fid)
         beta=1./T
         for _ in range(sweep_size):
-            new_action_protocol,new_hx_discrete=propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step)
+            new_action_protocol,new_hx_discrete=propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step,RL_constraint=RL_constraint)
             hx_discrete=new_hx_discrete
             
             new_fid=Fidelity(psi_i,H,N_time_step,delta_t,psi_target)
@@ -294,15 +421,13 @@ def simulate_anneal(params):
                 old_hx_discrete=new_hx_discrete
                 old_action_protocol=new_action_protocol
                 old_fid=new_fid
-            #else:
-            #    print("move rejected!",np.exp(beta*dF))
         
         step+=1.0
         T=Ti*(1.0-step/N_quench)
-      
+    
+    print("Performing 5 sweeps at zero-temperature")
     for _ in range(5*sweep_size): ## Perform greedy sweeps (zero-temperature):
-        #print(_)
-        new_action_protocol,new_hx_discrete=propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step)
+        new_action_protocol,new_hx_discrete=propose_new_trajectory(old_action_protocol,old_hx_discrete,hx_i,N_time_step,RL_constraint=RL_constraint)
         hx_discrete=new_hx_discrete
         new_fid=Fidelity(psi_i,H,N_time_step,delta_t,psi_target)
         count_fid_eval+=1
@@ -317,7 +442,16 @@ def simulate_anneal(params):
             old_action_protocol=new_action_protocol
             old_fid=new_fid
     print("Done")
+    
+    enablePrint()
     return count_fid_eval,best_fid,best_action_protocol,best_hx_discrete
+
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
 
 # Run main program !
 if __name__ == "__main__":
--- testing.py
+++ testing.py
@@ -3,13 +3,25 @@ import pickle
 from matplotlib import pyplot as plt
 
 #file=open("data/a1_t=30_fideval=500.pkl","rb")
-#file=open("data/marin.pkl","rb")
+file=open('data/BB_action_set_1.pkl','rb')
+#file=open('data/first_test.pkl','rb')
+data=pickle.load(file,encoding='latin1')
+print([data[i][1] for i in range(10)])
+#data=pickle.load(file)
+#print(data)
+exit()
+print(data[-1])
+data=data[-1][3]
+#print(data[0][2])
 #res1=pickle.load(file)
 #print(res1)
 
-data=[ 4.00000001,  4.00000001,  3.99999999,  3.99999999,  3.99999999,
-        3.99999996,  0.6       ,  2.39999997,  2.70000003, -0.80000004,
-       -1.39999998,  1.09999998, -3.3       , -1.3       , -4.00000004,
-       -3.99999999, -4.00000003, -4.00000004, -4.00000004, -4.00000001]
-plt.step(range(20),data,where='post')
+#data=data[2]
+#===============================================================================
+# data=[ 4.00000001,  4.00000001,  3.99999999,  3.99999999,  3.99999999,
+#         3.99999996,  0.6       ,  2.39999997,  2.70000003, -0.80000004,
+#        -1.39999998,  1.09999998, -3.3       , -1.3       , -4.00000004,
+#        -3.99999999, -4.00000003, -4.00000004, -4.00000004, -4.00000001]
+#===============================================================================
+plt.step(range(data.shape[0]),data,where='post')
 plt.show()  
