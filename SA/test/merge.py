910eaa2833865a112f2eb6195daa1a8b2c452718
Alexandre Day <alexandre.day1@gmail.com>
2017-03-11 14:01:38 -0500

    resync

--- SA/test/merge.py
+++ SA/test/merge.py
@@ -19,14 +19,17 @@ def main():
     n_elements = 2**n_step // tot_number_interval
     #hx_int = [n_elements*interval_slice, n_elements*(interval_slice+1)]
 
+    all_data=[]
     for interval_slice in range(tot_number_interval):
         param = {'L' : L, 'T': T, 'n_step': n_step, 'slice': interval_slice}
         file_name = make_file_name(param)
         with open(file_name,'rb') as f:
             data=pickle.load(f)
-    
-        mat_file=make_file_name(param,ext=".mat")
-        scipy.io.savemat(mat_file, mdict={'slice_%i'%interval_slice: data})
+            
+        all_data.append(data)
+
+    mat_file=make_file_name_2(param,ext=".mat")
+    scipy.io.savemat(mat_file, mdict={'data': np.concatenate(all_data)})
 
 
 
@@ -41,6 +44,15 @@ def make_file_name(param, root="",ext=".pkl",prefix=""):
     f = [k+"-"+key_format[k].format(param[k]) for k in sorted(key_format)]
     return root+prefix+'ES_'+"_".join(f)+ext
 
+def make_file_name_2(param, root="",ext=".pkl",prefix=""):
+    key_format = {
+        'L':'{:0>2}',
+        'T':'{:.3f}',
+        'n_step':'{:0>2}'
+    }
+
+    f = [k+"-"+key_format[k].format(param[k]) for k in sorted(key_format)]
+    return root+prefix+'ES_'+"_".join(f)+ext
 
 if __name__ == "__main__":
     main()
