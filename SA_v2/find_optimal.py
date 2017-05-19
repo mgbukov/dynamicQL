8c62acc4ad1a5268853c2c2bf85c39259391e202
alexandreday <alexandre.day1@gmail.com>
2017-05-19 16:26:55 -0400

    optimal search

--- SA_v2/find_optimal.py
+++ SA_v2/find_optimal.py
@@ -9,16 +9,14 @@ def main():
     
     utils=UTILS()
     parameters = utils.read_parameter_file()
-    
+    res = {} 
     for T in [0.1, 0.3, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]:
         for n_step in [4,6,8,10,12,14,16,18,20,22,24,26,28]:
-            
             parameters['T']= T
             parameters['n_step'] = n_step
             parameters['dt'] = T/n_step
             
             file = utils.make_file_name(parameters,root='data/')
-            res = {}
             with open(file,'rb') as f:
                 data = pickle.load(f)
             
@@ -27,9 +25,11 @@ def main():
             data[pos_min,0]-=10.
             pos_min_2 = np.argmax(data[:,0])
             gap = optimal_fid - data[pos_min_2,0]
-            print(optimal_fid,'\t',gap)
+            print(T,'\t',n_step,'\t',optimal_fid,'\t',gap)
             res[(T,n_step)]=[optimal_fid, gap]
-            exit()
+    with open('optimal.pkl','wb') as f:
+        pickle.dump(res,f)
+
 
 
 
